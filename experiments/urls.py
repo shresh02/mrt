892c57fe8beaff3ce12fa0c5d5248f17e0c6e1a4
mumbihere <monicahwambugu@gmail.com>
2018-01-08 15:22:23 -0800

    Dashboard

--- experiments/urls.py
+++ experiments/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^2$', views.index2, name='index2'),
     url(r'^sendmsg/(?P<prompt>.+)/$', views.send_message),
+    url(r'^dashboard/$',views.dashboard)
 
 
 ]
